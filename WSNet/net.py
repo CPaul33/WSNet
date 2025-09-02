0eef10ca1a6186780bf26f07dc2a41ff127c6765
Pengsen Lu <128160489+CPaul33@users.noreply.github.com>
2025-09-02 20:31:58 +0800

    Update net.py

--- WSNet/net.py
+++ WSNet/net.py
@@ -3,20 +3,22 @@ import torch.nn.functional as F
 from utils import *
 import os
 from loss import *
-from model import *
+from model.WSNet.WSNet import *
 from skimage.feature.tests.test_orb import img
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 class Net(nn.Module):
-    def __init__(self, model_name, mode):
+    def __init__(self, model_name, dataset_name ,mode):
         super(Net, self).__init__()
         self.model_name = model_name
         
         self.cal_loss = SoftIoULoss()
 
-        if model_name == 'WSNet':
+        if model_name == 'WSNet' and (dataset_name != 'NUDT-SIRST'):
             self.model = WSNet()
+        else:
+            self.model = WSNet_NUDT()
 
     def forward(self, img):
         return self.model(img)
@@ -24,3 +26,4 @@ class Net(nn.Module):
     def loss(self, pred, gt_mask):
         loss = self.cal_loss(pred, gt_mask)
         return loss
+
