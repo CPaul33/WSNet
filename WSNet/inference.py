26084b4ce21d46258a0961a5f5628397759d2b17
Pengsen Lu <128160489+CPaul33@users.noreply.github.com>
2026-02-04 11:00:01 +0800

    Refactor inference.py for model inference process

--- WSNet/inference.py
+++ WSNet/inference.py
@@ -11,8 +11,8 @@ from tqdm import tqdm
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 parser = argparse.ArgumentParser(description="WSNet Inference without mask")
-parser.add_argument("--model_names", default=['WSNet','ACM', 'ALCNet','DNANet', 'ISNet', 'RDIAN', 'ISTDU-Net'], nargs='+',
-                    help="model_name: 'WSNet','ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
+parser.add_argument("--model_names", default=['WSNet'], nargs='+',
+                    help="model_name: 'WSNet'")
 parser.add_argument("--pth_dirs", default=None, nargs='+',  help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
 parser.add_argument("--dataset_dir", default='/root/datasets', type=str, help="train_dataset_dir")
 parser.add_argument("--dataset_names", default=['SIRST', 'NUDT-SIRST', 'IRSTD-1K'], nargs='+',
@@ -103,6 +103,7 @@ if __name__ == '__main__':
                         opt.pth_dir = opt.save_log + pth_dir
                         test()
                         print('\n')
+
 
 
 
